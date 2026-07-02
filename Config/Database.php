81180c199c6fe9545f0c0861213bcb29dab5c745
Prima <suratprima@gmail.com>
2026-07-02 10:57:15 +0700

    change default host config database

--- Config/Database.php
+++ Config/Database.php
@@ -18,7 +18,7 @@ class Database
     public array $connections = [
         'default' => [
             'driver'    => 'mysql',
-            'host'      => 'localhost',
+            'host'      => '127.0.0.1',
             'port'      => '3306',
             'database'  => 'testdb',
             'username'  => 'root',
